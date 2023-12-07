d32ec7f14060c386959d35264d66acb68d42898e
M <marco.pulze.03@gmail.com>
2023-12-07 20:19:00 +0100

    add struct project and set up the main functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,57 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ops::Deref;
+// use robotics_lib::interface::Tools;
+use crate::projects::{Project, Shape, StopReason};
+mod projects;
+
+pub struct SalviniBot{
+}
+
+impl SalviniBot{
+    pub fn new() -> Self{
+        SalviniBot{ }
+    }
+    //ti dice qunanto vai a sependere (non so se fare passando il robot o passando la cacca addosso), non mi serve a nulla self i guess
+    pub fn design_project(&self, posizione: (usize, usize), shape: Shape, map_size: usize) ->Result<Project, Project>{
+        let mut rock_needs:u32 = 0;
+        let mut energy_needs: usize = 0;
+        match shape {
+            Shape::Square(x) => {
+
+            }
+            Shape::Rectangle(x, y) => {}
+            Shape::Roundabout(d) => {}
+            Shape::Cross(l) => {}
+            Shape::LongLong(l) => {}
+            Shape::LShape(l, s) => {}
+        }
+        todo!()
+    }
+    //need to add the checks in the robot
+    pub fn check_project(project: Project)->Result<Project, StopReason>{
+        let robot_energy = 0;
+        let robot_rock = 0;
+        if project.cost > robot_energy{ return Err(StopReason::LowEnergy); }
+        if project.rock > robot_rock { return Err(StopReason::MissingRocks) }
+        Ok(project)
+        // Err(StopReason::PonteSulloStretto)
+    }
+    pub fn road_building(){
+        todo!()
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    //it compiles but not work as i thought lol
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        println!("it does not");
+        let newshape = Shape::Square(6);
+        print!("{:?}",newshape.get_action_curve());
+
     }
 }
+
---
+++ src/projects.rs
@@ -0,0 +1,141 @@
+use std::ops::Deref;
+
+// use robotics_lib::interface::Direction;
+//The SalviniTool before building a new type of road must create a project to analise it and understand its need.
+//the Project struct contains:
+//-action_curves:that describe the path the robot is going to follow to build the road
+//-cost: the amount of energy it is going to take to build the whole project
+//-rock: the amount of rock it is going to need to build the project
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Direction{
+    Up,
+    Down,
+    Left,
+    Right
+}
+//Error type given to the user after checking a designed project, and whever there is an
+//interaction in the road building functon
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum StopReason{
+    LowEnergy,
+    MissingRocks,
+    PonteSulloStretto
+}
+pub struct Project{
+    pub(crate) curves_action: Vec<Direction>,
+    pub(crate) cost: u32,
+    pub(crate) rock: u32,
+}
+//ragruppo maybe?
+pub struct UnfinishedProject{
+    pub(crate) start_position: (usize,usize),
+    pub(crate) curves_action: Vec<Direction>,
+    pub(crate) cost: u32,
+    pub(crate) rock: u32,
+}
+
+pub enum Shape{
+    Square(u32),
+    Rectangle(i32, i32),
+    Roundabout(i32),
+    Cross(i32),
+    LongLong(i32),
+    LShape(i32, i32)
+}
+
+//this mirrors the shape(for the circle/roundabout, but could also be implemented for a
+pub(crate) fn mirror_direction(vec: &Vec<Direction>, angle: u32)->Vec<Direction>{
+    let mut v:Vec<Direction> = vec![];
+    match angle{
+        0 => {
+            for d in vec.len()-1 ..0 {
+                if vec[d] == Direction::Right{
+                    v.push(Direction::Up);
+                }
+                if vec[d] == Direction::Up{
+                    v.push(Direction::Right);
+                }
+            }
+        }
+        1 => {
+            for d in 0..vec.len()-1 {
+                if vec[d] == Direction::Right{
+                    v.push(Direction::Left);
+                }
+            }
+        }
+        2 =>{
+            for d in vec.len()-1 ..0 {
+                if vec[d] == Direction::Right{
+                    v.push(Direction::Left);
+                }
+                if vec[d] == Direction::Up{
+                    v.push(Direction::Down);
+                }
+                if vec[d] == Direction::Down{
+                    v.push(Direction::Up);
+                }
+                if vec[d] == Direction::Left{
+                    v.push(Direction::Right);
+                }
+            }
+        }
+        _ => { }
+    }
+    v
+}
+pub(crate) fn complete_shape(vec: &mut Vec<Direction>){
+    for i in 0..2 {
+        vec.append(&mut mirror_direction(&vec, i));
+    }
+}
+// impl From<UnfinishedProject> Project{
+//     fn from(){
+//
+//     }
+// }
+
+impl Shape{
+    pub fn get_action_curve(&self) ->Vec<Direction>{
+        let mut v:Vec<Direction> = vec![];
+        match self {
+            Shape::Square(side_lenght) => {
+                let x = *side_lenght as i32;
+                for i in 0..x/2{
+                    v.push(Direction::Right);
+                }
+                for i in 0..x {
+                    v.push(Direction::Up);
+                }
+                for i in 0..x/2{
+                    v.push(Direction::Left);
+                }
+                let mut other = mirror_direction(&v, 2);
+                v.append(&mut other);
+            }
+            Shape::Rectangle(x, y) => {
+                for i in 0..x/2{
+                    v.push(Direction::Right);
+                }
+                for i in 0..*y{
+                    v.push(Direction::Up);
+                }
+                for i in 0..x/2{
+                    v.push(Direction::Left);
+                }
+                let other = mirror_direction(&v, 2);
+            }
+            Shape::Roundabout(d) => {todo!()}
+            Shape::Cross(l) => {
+
+            }
+            Shape::LShape(l, s) =>{
+
+            }
+            Shape::LongLong(L) => {
+
+            }
+        }
+        v
+    }
+}
